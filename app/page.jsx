76d616905dd7aecff3b9b6135dcf26c4fadcb3e8
Bonniezilla <bonnieevil86@gmail.com>
2023-09-04 09:54:59 -0300

    Adding the provider

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,12 @@
-import "./globals.css"; 
+import "./globals.css";
 
-import Provider from "./components/Provider";
 import SignInButton from "./components/SignInButton";
 
 export default function Home() {
   return (
-    <Provider>
-      <div className="flex justify-center items-center h-screen w-screen flex-col gap-4">
-        <h1 className="font-bold">Google Auth Test</h1>
-        <SignInButton />
-      </div>
-    </Provider>
+    <div className="flex justify-center items-center h-screen w-screen flex-col gap-4">
+      <h1 className="font-bold">Google Auth Test</h1>
+      <SignInButton />
+    </div>
   )
 }
